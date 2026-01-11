71526d482efb893529a4d7f4301e54c71bf91c4e
AstroJaggaDaku <iamastrojd@gmail.com>
2026-01-11 19:20:56 +0530

    Update download-swiss.js

--- scripts/download-swiss.js
+++ scripts/download-swiss.js
@@ -1,24 +1,33 @@
-import fs from fs;
-import https from https;
-import path from path;
-
-const base = httpsraw.githubusercontent.comptprashanttripathisweph-wasmmaindist;
+import fs from "fs";
+import https from "https";
+import path from "path";
 
 const files = {
-  publicswisseph.js `${base}swisseph.js`,
-  publicswisseph.wasm `${base}swisseph.wasm`,
-  publicepheseas_18.se1 httpsraw.githubusercontent.comaloistrswissephmasterepheseas_18.se1,
-  publicephesemo_18.se1 httpsraw.githubusercontent.comaloistrswissephmasterephesemo_18.se1,
-  publicephesepl_18.se1 httpsraw.githubusercontent.comaloistrswissephmasterephesepl_18.se1
+  "public/swisseph.js":
+    "https://raw.githubusercontent.com/ptprashanttripathi/sweph-wasm/main/dist/swisseph.js",
+  "public/swisseph.wasm":
+    "https://raw.githubusercontent.com/ptprashanttripathi/sweph-wasm/main/dist/swisseph.wasm",
+  "public/ephe/seas_18.se1":
+    "https://raw.githubusercontent.com/aloistr/swisseph/master/ephe/seas_18.se1",
+  "public/ephe/semo_18.se1":
+    "https://raw.githubusercontent.com/aloistr/swisseph/master/ephe/semo_18.se1",
+  "public/ephe/sepl_18.se1":
+    "https://raw.githubusercontent.com/aloistr/swisseph/master/ephe/sepl_18.se1"
 };
 
 function download(url, file) {
-  return new Promise(res = {
-    fs.mkdirSync(path.dirname(file), { recursivetrue });
-    https.get(url, r = r.pipe(fs.createWriteStream(file)).on(finish, res));
+  return new Promise((resolve, reject) => {
+    fs.mkdirSync(path.dirname(file), { recursive: true });
+    https.get(url, res => {
+      if (res.statusCode !== 200) return reject(url);
+      res.pipe(fs.createWriteStream(file))
+        .on("finish", resolve);
+    });
   });
 }
 
-await Promise.all(Object.entries(files).map(([f,u])=download(u,f)));
+await Promise.all(
+  Object.entries(files).map(([file, url]) => download(url, file))
+);
 
-console.log(Swiss Ephemeris WASM Installed);
+console.log("Swiss Ephemeris WASM Installed");
